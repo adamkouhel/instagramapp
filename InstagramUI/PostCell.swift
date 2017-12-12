0c956e3f4b76720075e4e4996ff85378d7e65727
adam <32176844+adamkouhel@users.noreply.github.com>
2017-12-11 23:59:38 -0600

    Initial Commit

---
+++ InstagramUI/PostCell.swift
@@ -0,0 +1,40 @@
+//
+//  PostCell.swift
+//  InstagramUI
+//
+//  Created by Adam Kouhel on 12/11/17.
+//  Copyright © 2017 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    @IBOutlet weak var numberOfLikesButton: UIButton!
+    
+    @IBOutlet weak var postCaptionLabel: UILabel!
+    
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    
+    var post: Post! {
+        
+        didSet {
+            
+            self.updateUI()
+        }
+    }
+    
+    func updateUI()
+    {
+        self.postImageView.image = post.image
+        
+        postCaptionLabel.text = post.caption
+        
+        numberOfLikesButton.setTitle("Be the first one to share a comment", for: [])
+        
+        timeAgoLabel.text = post.timeAgo
+    }
+    
+}
